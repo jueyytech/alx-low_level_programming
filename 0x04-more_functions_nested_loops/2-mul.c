f742be09158d7e26a7b908b8fbb915e8a18ee929
Emmanuel Johnson <hiemmanuelumoke@gmail.com>
2023-02-23 16:53:23 -0800

    C source algorithms and source codes to check for digits,multiply digits,print numbers,print most numbers,print line,print triangle,print diagonal,print square and print triangle

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* mul - multiplies two integers
+* @i: first integer
+* @j: second integer
+* Return: multiplication of i and j
+*/
+int mul(int i, int j)
+{
+	return (i * j);
+}
